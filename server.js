aec630b3015d8e6df805addb9b56a32cc53f5c3c
nandhinisathish <39171961+nandhinisathish@users.noreply.github.com>
2025-10-15 13:32:37 -0400

    Initial commit with basic server set up

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 // Imports
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 
 // Set up
+dotenv.config();
+
+// Environment variables
+const PORT = process.env.PORT || 3000;
+const app = express();
+
 
 // Connect DB
 
 // Middleware
+app.use(express.json());
+app.use((req, _res, next)=>{
+    console.log(`${req.method} - ${req.path}`);
+    next();
 
+})
 // Routes
 
 // Global error handling
+app.use((err, _req, res, _next)=>{
+
+    res.json({msg: `Error - ${err.message}`});
+
+})
 
 // Server listener
+app.listen(PORT, ()=>{
+
+    console.log(`Server running on the PORT: ${PORT}`);
+})
